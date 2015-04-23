ec298b0415625cc2db9b1b91eac26b0fedae6b36
xiaoyawei <xiaoya@Xiaoyas-MacBook-Pro.local>
2015-04-22 22:11:52 -0400

    Initial Commit

---
+++ VRPD/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VRPD
+//
+//  Created by Xiaoya Wei on 4/22/15.
+//  Copyright (c) 2015 Xiaoya Wei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
