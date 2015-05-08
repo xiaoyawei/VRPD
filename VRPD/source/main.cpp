c1d850635be0721bebd0da4cf9c9f65b52e7147a
xiaoyawei <xiaoya@Xiaoyas-MacBook-Pro.local>
2015-05-08 16:31:59 -0400

    fix the prompts

--- VRPD/source/main.cpp
+++ VRPD/source/main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char * argv[]) {
     cmdline::parser parser;
     parser.add<string>("filename", 'f', "The name of the input file", true, "");
     parser.add("verbose", 'v', "Display the best solution at runtime");
-    parser.add<double>("stop", 's', "Set the threshold so that the program is terminated when a solution found better than stop is found", false, DefaultValue::stopObject);
+    parser.add<double>("stop", 's', "Set the threshold to terminate when a solution better than stop is found", false, DefaultValue::stopObject);
     parser.add<double>("startTemp", 't', "The starting temperature for the simulated annealing algorithm", false, DefaultValue::startingTemp);
     parser.add<double>("coolRatio", 'c', "The cooling ratio for the simulated annealing algirhtm", false, DefaultValue::coolingRatio);
     parser.add<int>("numLoop", 'l', "The number of loops for simulated annealing to run", false, DefaultValue::numLoop);
